ffebb8cf3ff0856dd0974dec16a9bc806dc3d02c
USER <bahrudinrizky313@gmail.com>
2019-09-16 17:30:09 +0700

    finish parcelable activity intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,20 +3,28 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+
+    private EditText usernameInput;
+    private EditText nameInput;
+    private EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_parcelable);
+        usernameInput = findViewById(R.id.input_username);
+        nameInput = findViewById(R.id.input_name);
+        ageInput = findViewById(R.id.input_age);
 
         // TODO: bind here
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
-            // TODO: display value here
+            User u = extras.getParcelable(key "user");
         }
     }
 }
