dc06b28f66a44c86e2e5f0497f9cf08b94c51130
USER <bahrudinrizky313@gmail.com>
2019-09-16 14:36:04 +0700

    finish implicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -23,4 +23,9 @@ public class MainActivity extends AppCompatActivity {
     public void handleExit(View view) {
         finish();
     }
+
+    public void handleImplicitIntent(View view) {
+        Intent intent = new Intent(this, ImplicitIntentActivity.class);
+        startActivity(intent);
+    }
 }
